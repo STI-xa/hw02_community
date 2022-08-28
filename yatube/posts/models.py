2cd9739c903783c157ab8043a82e10c3a291ff9e
Полина Николаева <sti-xa@ya.ru>
2022-08-28 11:11:09 +0300

    fixed bugs after website tests

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,7 +1,4 @@
-from turtle import title
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,15 +5,7 @@ from .models import Post, Group
 # Страница c постами, отфильтрованными по группам.
 def group_posts(request, slug):
     title = 'Записи сообщества'
-    # Функция get_object_or_404 получает по заданным критериям объект 
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
     group = get_object_or_404(Group, slug=slug)
-
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
